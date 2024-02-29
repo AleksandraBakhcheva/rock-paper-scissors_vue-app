fd2d3d17db9867ee36336e0f762acfc1344b6c28
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-02-29 18:58:05 +0300

    add Header component and styles

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="header">
+    <div class="header-logo">
+      <img
+        class="logo-original"
+        src="../assets/images/logo.svg"
+        alt="rock-paper-scissors"
+      />
+      <img
+        class="logo-bonus"
+        src="../assets/images/logo-bonus.svg"
+        alt="rock-paper-scissors-lizard-spock"
+      />
+    </div>
+    <div class="header-score">
+      <span class="header-score-text">Счет</span>
+      <span class="header-score-counter original-score">{{
+        $store.state.counter
+      }}</span>
+      <span class="header-score-counter bonus-score">{{
+        $store.state.bonusCounter
+      }}</span>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style lang="scss">
+@import "../assets/styles/Header.scss";
+</style>
