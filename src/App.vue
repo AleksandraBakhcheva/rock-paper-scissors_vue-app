d71b4d78424b9595205fa312977c426eccaf0daa
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-03-01 00:19:44 +0300

    add App and main.js

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <Header />
+  <main>
+    <vue-page-transition name="zoom"
+      ><router-view @item="item = $event" :itemSelected="this.item"
+    /></vue-page-transition>
+  </main>
+  <Footer />
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {
+      item: null,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./assets/styles/General.scss";
+</style>
